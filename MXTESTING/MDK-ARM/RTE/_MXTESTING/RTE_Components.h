681e2d93acff5e089a7c6d82a36d0b36e0f8e905
pietergoos <technococktail@gmail.com>
2019-04-25 22:24:01 +0200

    Added Testing folder for CubeMX

---
+++ MXTESTING/MDK-ARM/RTE/_MXTESTING/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'MXTESTING' 
+ * Target:  'MXTESTING' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
